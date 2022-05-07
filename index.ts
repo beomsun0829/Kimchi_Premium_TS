c10b549a14b20d849c4d32b3d1e08dbd53239113
beomsun0829 <ghsqjatns@naver.com>
2022-05-08 00:23:11 +0900

    comp

--- index.ts
+++ index.ts
@@ -6,12 +6,25 @@ import config from './config.json';
 const MarketList = config.MarketList;
 const TickerException = config.Ticker_Exception;
 
+console.log("-----------------------------------------------------");
+console.log('MarketList:', MarketList);
+console.log('Ticker Exception : ', TickerException);
+
 let Fetched_Tickers = {};
 let Tickers_in_MarketList = {};
 let Symbol_List = {};
 let Refined_Tickers = {};
 
-main();
+let tether_price = -1;
+
+loop();
+
+async function loop(){
+    while(true){
+        await main();
+        console.log("-----------------------------------------------------");
+    }
+}
 
 async function main() {
     await initialize();
@@ -20,13 +33,14 @@ async function main() {
     await GetSymbolList();
     await DeleteOneMarketSymbol();
     await GetTickersBySymbol();
-    debuglog(Refined_Tickers);
+    await GetTetherPrice();
+    await CalcKrwToTether();
+    await CalcPremium();
+
 }
 
 async function initialize() {
     //console.log(ccxt.exchanges);
-    console.log('MarketList:', MarketList);
-    console.log('Ticker Exception : ', TickerException);
 
     for (let key in MarketList) {
         await GetTickers(key);
@@ -101,6 +115,9 @@ async function GetTickersBySymbol(){
         let tickers = Fetched_Tickers[exchange];
         for(let symbol in Symbol_List){
             if(Symbol_List[symbol].includes(exchange + "_" + MarketList[exchange])){
+                if(tickers[symbol + "/" + MarketList[exchange]]['close'] == 0)
+                    continue;
+
                 if(Refined_Tickers[symbol][exchange + "_" + MarketList[exchange]] == undefined)
                     Refined_Tickers[symbol][exchange + "_" + MarketList[exchange]] = tickers[symbol + "/" + MarketList[exchange]]['close'];
                 
@@ -109,6 +126,57 @@ async function GetTickersBySymbol(){
             }
         }
     }
+}
+
+async function GetTetherPrice(){
+    tether_price = Refined_Tickers['BTC']['upbit_KRW'] / Refined_Tickers['BTC']['binance_USDT']
+    console.log('Tether Price:', tether_price);
+}
+
+async function CalcKrwToTether(){
+    if(tether_price == -1){
+        console.log('Tether Price is not found');
+    }
+
+    for(let symbol in Refined_Tickers){
+        for(let index in Refined_Tickers[symbol]){
+            let market = index.split('_')[1];
+            if(market == 'KRW')
+                Refined_Tickers[symbol][index] = Refined_Tickers[symbol][index] / tether_price;
+        }
+    }
+}
+
+function CalcPremium(){
+    for(let symbol in Refined_Tickers){
+        if(CheckSymbolException(symbol))
+            continue;
+
+        let maxval : number = Math.max.apply(null, Object.values(Refined_Tickers[symbol]));
+        let maxindex = Object.keys(Refined_Tickers[symbol]).find(key => Refined_Tickers[symbol][key] === maxval);
+        let minval : number = Math.min.apply(null, Object.values(Refined_Tickers[symbol]));
+        let minindex = Object.keys(Refined_Tickers[symbol]).find(key => Refined_Tickers[symbol][key] === minval);
+        let premium : number = ((maxval / minval) - 1 ) * 100;
+
+        //debuglog("debug : " + symbol + " : " + maxval + " : " + minval + " : " + premium);
+
+
+        if(Math.abs(premium) > 5){
+            console.log(symbol + " | 5% 이상 차이 " + "( " + premium.toFixed(3) + " % )" + " | " + minindex + " -> " + maxindex);
+        }
+
+        else if(Math.abs(premium) > 3){
+            console.log(symbol + " | 3% 이상 차이 " + "( " + premium.toFixed(3) + " % )" + " | " + minindex + " -> " + maxindex);
+        }
+        
+    }
+}
+
+function CheckSymbolException(symbol){
+    if(TickerException.includes(symbol))
+        return true;
+    else
+        return false;
 }
 
 async function debuglog(str){
